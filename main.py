c0a53410db3478f363086234f7a4579f2a74eeeb
watermelonDrip <69283174+watermelonDrip@users.noreply.github.com>
2021-11-10 17:56:38 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
@@ -11,6 +8,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Hello World')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ 
